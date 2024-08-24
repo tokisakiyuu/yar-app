c0fbaffc97fc6e8e4e5090c4c45276f1fcd557bd
tokisakiyuu <tokisakiyuu@qq.com>
2024-08-24 21:30:02 +0800

    base layout

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import RecordList from './components/RecordList'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World !</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <RecordList />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   )
 }
 
