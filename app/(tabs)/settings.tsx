a799233d9e6cb55e34e4128e60658f2f078ceaa1
tokisakiyuu <tokisakiyuu@qq.com>
2024-09-18 22:22:28 +0800

    update

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,15 +1,10 @@
-import * as DocumentPicker from 'expo-document-picker'
 import { Button, View } from 'react-native'
 
 export default function Page() {
-  const handleSelect = () => {
-    DocumentPicker.getDocumentAsync()
-  }
-
   return (
     <View>
       <View>
-        <Button title="Select Folder" onPress={handleSelect} />
+        <Button title="Select Folder" />
       </View>
     </View>
   )
